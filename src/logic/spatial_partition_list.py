1735f7554416e88f08b4a1e0cc381674bee15228
Tr0im0n <129655960+Tr0im0n@users.noreply.github.com>
2025-10-31 16:52:37 +0100

    Can only instatntiate not change list

--- src/logic/spatial_partition_list.py
+++ src/logic/spatial_partition_list.py
@@ -3,15 +3,13 @@ import numpy as np
 
 
 class SpatialPartitionList:
+    """ Can't be changed after instantiation. """
     N_NEIGHBORS = 9
     
-    sim_width: int
-    sim_height: int
-    total_cells: int
     grid_width: int
     grid_height: int
+    total_cells: int
     
-    partition_list: list
     neighboring_cells: np.ndarray
     
 #########################################################################################################################
@@ -19,104 +17,90 @@ class SpatialPartitionList:
 #########################################################################################################################
     
     def __init__(self,
-        cell_size: int,
-        sim_width: int,
-        sim_height: int,
+            particles: np.ndarray,
+            cell_size: int,
+            sim_width: int,
+            sim_height: int,
         ):
         
+        self._partition_list = []
+        
+        self.cell_size = cell_size
         self.sim_width = sim_width
         self.sim_height = sim_height
         
-        self.cell_size = cell_size
+        self.calc_vals()
+        self.create_neighboring_cells_array()
+        self.populate_spatial_partition(particles)
         
-        self.create_new_partition_list()
         
     def __getitem__(self, key: int):
         """ Allows instance[index] or instance[slice] to access elements of self.main_list. """
-        return self.partition_list[key]
+        return self._partition_list[key]
     
     def __iter__(self):
         """Returns an iterator over the internal list."""
-        return iter(self.partition_list)
-    
-#########################################################################################################################
-# Properties
-#########################################################################################################################
-    
-    @property
-    def cell_size(self) -> float:
-        return self._cell_size
-    
-    @cell_size.setter
-    def cell_size(self, value: float) -> None: 
-        self._cell_size = value
-        self._calc_vals_dependent_on_cell_size()
-
-    def _calc_vals_dependent_on_cell_size(self) -> None:
-        # Grid dimensions
-        self.grid_width = int(self.sim_width // self.cell_size)
-        self.grid_height = int(self.sim_height // self.cell_size)
-        self.total_cells = self.grid_width * self.grid_height
-        # 
-        self.neighbor_offsets = np.array([
-            -self.grid_width -1, -self.grid_width, -self.grid_width +1,
-            -1,                 0,              1,
-            self.grid_width -1,  self.grid_width, self.grid_width +1
-        ])
-        self.create_neighboring_cells_array()
-        print(self.neighboring_cells)
+        return iter(self._partition_list)
 
 #########################################################################################################################
 # Instance Methods
 #########################################################################################################################
     
+    def calc_vals(self) -> None:
+        """ Only used once in the init. """
+        # Grid dimensions
+        self.grid_width = int(self.sim_width // self.cell_size)
+        self.grid_height = int(self.sim_height // self.cell_size)
+        self.total_cells = self.grid_width * self.grid_height
+        # neighbor info
+        self.neighbor_offsets = np.array([
+            -self.grid_width -1, -self.grid_width, -self.grid_width +1,
+            -1,                 0,              1,
+            self.grid_width -1,  self.grid_width, self.grid_width +1
+        ])
+        
     def get_partition_index_from_pos(self, point: np.ndarray) -> int:
         """ Get the index in the spatial partition, of the particle. """
         cell_x = int(point[0] // self.cell_size)
         cell_y = int(point[1] // self.cell_size)
         return cell_y * self.grid_width + cell_x
 
-    def create_new_partition_list(self):
-        """ Read function name. """
-        self.partition_list = [list() for _ in range(self.total_cells)]
-
-    def populate_spatial_partition(self, particles: np.ndarray, create_new_list: bool = False) -> None:
-        """ Put the particles indices in their correct spatial partition. """
-        if create_new_list:
-            self.create_new_partition_list()
-
+    def populate_spatial_partition(self, particles: np.ndarray) -> None:
+        """ Only used once in the init. """
         for particle_index, particle in enumerate(particles):
             cell_index = self.get_partition_index_from_pos(particle)
-            self.partition_list[cell_index].append(particle_index)
+            self._partition_list[cell_index].append(particle_index)
+
+    def calc_neighbor_indices_of_cell(self, cell_index: int):
+        """ Only used in next method. """
+        current_col = cell_index % self.grid_width
+        last_col = self.grid_width - 1
+        
+        if current_col == 0:
+            offsets = self.neighbor_offsets[[1, 2, 4, 5, 7, 8]]
+        elif current_col == last_col:
+            offsets = self.neighbor_offsets[[0, 1, 3, 4, 6, 7]]
+        else:
+            offsets = self.neighbor_offsets     
+        
+        potential_indices = cell_index + offsets
+        mask = (potential_indices >= 0) & (potential_indices < self.total_cells)
+        return potential_indices[mask]
 
     def create_neighboring_cells_array(self):
-        """ Create array where each row are the indices of the cells neighboring the cell of that index """
+        """ Create array where each row are the indices of the cells neighboring the cell of that index. 
+            Only used once in the init. """
         self.neighboring_cells = np.full((self.total_cells, self.N_NEIGHBORS), -1, dtype=np.int32)
-        for i in range(self.total_cells):
-            # neighbor_cell_indices = self.neighbor_offsets + i
-            current_col = i % self.grid_width
-            last_col = self.grid_width - 1
-            
-            if current_col == 0:
-                offsets = self.neighbor_offsets[[1, 2, 4, 5, 7, 8]]
-            elif current_col == last_col:
-                offsets = self.neighbor_offsets[[0, 1, 3, 4, 6, 7]]
-            else:
-                offsets = self.neighbor_offsets     
-            
-            neighbor_cell_indices = [
-                i + offset
-                for offset in offsets
-                if 0 <= (i + offset) < self.total_cells
-            ]
+        for cell_index in range(self.total_cells):
+            neighbor_cell_indices = self.calc_neighbor_indices_of_cell(cell_index)
             n_neighbors = len(neighbor_cell_indices)
-            self.neighboring_cells[i, 0:n_neighbors] = neighbor_cell_indices
+            self.neighboring_cells[cell_index, 0:n_neighbors] = neighbor_cell_indices
 
     def get_neighboring_particle_indices(self, point: np.ndarray) -> np.ndarray:
         """ Returns the indices of the particles in the neighboring partitions. """
         partition_index = self.get_partition_index_from_pos(point)
         neighbor_cell_indices = self.neighboring_cells[partition_index]
-        list_of_lists_of_particle_indices = [self.partition_list[i] for i in neighbor_cell_indices]
+        list_of_lists_of_particle_indices = [self._partition_list[i] for i in neighbor_cell_indices]
         # np.concatenate apparently doesn't work with empty lists
         non_empty_lists = [lst for lst in list_of_lists_of_particle_indices if lst]
         if not non_empty_lists:
--- src/view/test_itertools.py
+++ src/view/test_itertools.py
@@ -3,14 +3,28 @@ import numpy as np
 from itertools import chain
 
 
-a = np.array([[1,2],[3,4],[5,6]])
-
-b = np.arange(24).reshape(4, 3, 2)
-
-flat_iterator = chain.from_iterable(b)
-
-particles_to_check = np.fromiter(flat_iterator, dtype=np.intp)
-
-print(particles_to_check)
+class TestClass:
+    def __init_(self):
+        instance_var = 2
 
 
+def test_itertools():
+    a = np.array([[1,2],[3,4],[5,6]])
+    b = np.arange(24).reshape(4, 3, 2)
+    flat_iterator = chain.from_iterable(b)
+    particles_to_check = np.fromiter(flat_iterator, dtype=np.intp)
+    print(particles_to_check)
+
+
+def test_class_indexing():
+    test_instance = TestClass()
+    # test_instance[2]
+
+
+def main():
+    # test_itertools()
+    test_class_indexing()
+
+
+if __name__ == "__main__":
+    main()
